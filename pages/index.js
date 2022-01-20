af07aac4ee0d54229b23b4866884a610ee0145f9
Denis Nallbati <dnallbati@gmail.com>
2022-01-20 21:13:44 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ import Header from "./components/header/Header";
 
 export default function Home({ smallCards, mediumCards }) {
   return (
-    <div className="r">
+    <div className="r flex flex-col justify-center">
       <Head>
         <title>Airbnb Clone</title>
         <link rel="icon" href="/favicon.ico" />
@@ -18,7 +18,7 @@ export default function Home({ smallCards, mediumCards }) {
 
       <Banner />
 
-      <main className="flex flex-col justify-center max-w-8xl mx-auto px-8 sm:px-16">
+      <main className=" max-w-8xl mx-auto px-8 sm:px-16">
         {/* Small Cards Section */}
         <section className="pt-5 mt-3 max-w-7xl">
           <h2 className="text-4xl font-semibold pb-5">Explore nearby</h2>
